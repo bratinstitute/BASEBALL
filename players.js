bb622699a423e99dbee1d99c57b4c9f32a579b2b
lisiswebsite <lisi222@gmail.com>
2024-11-29 22:53:28 -0500

    mitch

--- players.js
+++ players.js
@@ -63,7 +63,7 @@ const players = [
         bioLink: 'http://bratinstitute.com'},
     { no: 32, name: "Jurrangelo Kryszczuk", pos: "RHP", year: "Junior", ht: "6'5\"", wt: "220 lbs", hometown: "Gurnee, Ill.", school: "Warren Township HS", bt: "R/R", image: "images/32 Jurrangelo Kryszczuk.png",
         bioLink: 'http://bratinstitute.com'},
-    { no: 33, name: "Mitch Grinsell", pos: "C", year: "Senior", ht: "6'1\"", wt: "205 lbs", hometown: "Sugar Land, Texas", school: "Fort Bend Clements HS", bt: "R/R", image: "images/33 Mitch Grinsell.jpg",
+    { no: 33, name: "Mitch Grinsell", pos: "C", year: "Senior", ht: "6'1\"", wt: "205 lbs", hometown: "Sugar Land, Texas", school: "Fort Bend Clements HS", bt: "R/R", image: "images/33 Mitch Grinsell.png",
         bioLink: 'http://bratinstitute.com'},
     { no: 34, name: "Matt Pennington", pos: "INF", year: "Junior", ht: "6'2\"", wt: "210 lbs", hometown: "Rockaway, N.J.", school: "Morris Knolls HS", bt: "R/R", image: "images/34 Matt Pennington.jpg",
         bioLink: 'http://bratinstitute.com'},
